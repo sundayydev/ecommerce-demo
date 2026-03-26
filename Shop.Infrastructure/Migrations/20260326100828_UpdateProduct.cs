8434a27471df165e1daaad2d751f63c2b98036bc
sundayydev <sundayy.dev@gmail.com>
2026-03-26 23:45:54 +0700

    fix: Update rule for Prouct Variant

---
+++ Shop.Infrastructure/Migrations/20260326100828_UpdateProduct.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+        }
+    }
+}
---
+++ Shop.Infrastructure/Migrations/20260326161104_UpdateProductVariant.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductVariant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProductVariants_ProductId",
+                table: "ProductVariants");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductVariant_Product_Size_Color",
+                table: "ProductVariants",
+                columns: new[] { "ProductId", "Size", "Color" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProductVariant_Product_Size_Color",
+                table: "ProductVariants");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductVariants_ProductId",
+                table: "ProductVariants",
+                column: "ProductId");
+        }
+    }
+}
